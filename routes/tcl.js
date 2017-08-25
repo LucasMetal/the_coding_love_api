06ee04b55f149b9e11de44037f2b58b35db8b92c
Lucas G. Devescovi <lucas.devescovi@gmail.com>
2017-08-25 21:38:07 +0200

    Add search endpoint using Google

--- routes/tcl.js
+++ routes/tcl.js
@@ -16,4 +16,20 @@ router.post('/random/', function (req, res, next) {
     });
 });
 
+/* POST search. */
+router.post('/search/', function (req, res, next) {
+
+    // Here we could check for the slack token, to validate that it's our integration
+
+    var searchText = req.body.text;
+
+    TCL.search(searchText)
+      .then((response) => {
+        res.status(200).json(response);
+      })
+      .catch((error) => {
+        res.status(500).send(error);
+      });
+  });
+
 module.exports = router;
